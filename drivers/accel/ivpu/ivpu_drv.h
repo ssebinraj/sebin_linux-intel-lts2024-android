bba35049b58bb407cf223f46720d928c6b048996
Zhao, Shirley <shirley.zhao@intel.com>
2025-05-27 09:18:01 +0800

    Revert "accel/ivpu: Add initial Panther Lake support"
    This reverts commit eea990e874d93c8184cc45639c620f6de4b9eb63.

--- drivers/accel/ivpu/ivpu_drv.h
+++ drivers/accel/ivpu/ivpu_drv.h
@@ -23,10 +23,9 @@
 #define DRIVER_DESC "Driver for Intel NPU (Neural Processing Unit)"
 #define DRIVER_DATE "20230117"
 
-#define PCI_DEVICE_ID_MTL	0x7d1d
-#define PCI_DEVICE_ID_ARL	0xad1d
-#define PCI_DEVICE_ID_LNL	0x643e
-#define PCI_DEVICE_ID_PTL_P	0xb03e
+#define PCI_DEVICE_ID_MTL   0x7d1d
+#define PCI_DEVICE_ID_ARL   0xad1d
+#define PCI_DEVICE_ID_LNL   0x643e
 
 #define IVPU_HW_IP_37XX 37
 #define IVPU_HW_IP_40XX 40
@@ -216,8 +215,6 @@ static inline int ivpu_hw_ip_gen(struct ivpu_device *vdev)
 		return IVPU_HW_IP_37XX;
 	case PCI_DEVICE_ID_LNL:
 		return IVPU_HW_IP_40XX;
-	case PCI_DEVICE_ID_PTL_P:
-		return IVPU_HW_IP_50XX;
 	default:
 		dump_stack();
 		ivpu_err(vdev, "Unknown NPU IP generation\n");
@@ -232,7 +229,6 @@ static inline int ivpu_hw_btrs_gen(struct ivpu_device *vdev)
 	case PCI_DEVICE_ID_ARL:
 		return IVPU_HW_BTRS_MTL;
 	case PCI_DEVICE_ID_LNL:
-	case PCI_DEVICE_ID_PTL_P:
 		return IVPU_HW_BTRS_LNL;
 	default:
 		dump_stack();
